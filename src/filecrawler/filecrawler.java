a0980ddac2f698f0fb0c5628a4f76c5f9e9607f2
mhema <mhema@192.168.7.120>
2022-08-27 23:37:11 +0200

    small fix to match output as assignment says

--- src/filecrawler/filecrawler.java
+++ src/filecrawler/filecrawler.java
@@ -7,48 +7,38 @@ import java.util.Scanner;
 public class filecrawler {
 	
 	public static void main(String[] args) {
-		
 		Scanner inp = null;
 		String searchText = null;
 		
 		inp = new Scanner(System.in);
 		System.out.print("Enter searchtext - ");  
 		searchText= inp.next();
-	
 		if (inp != null) {
 			inp.close();
 		}
 		
-		readFile("c:\\filecrawler\\röd.txt", searchText);
-		
+		readFile("c:\\filecrawler\\röd.txt", searchText);	
 	}
 	
-	protected static void readFile(String filepath, String searchText) {
+	private static void readFile(String filepath, String searchText) {
 		File file = new File(filepath);
-		
 		Scanner scanner = null;
 		
 		try {
 			scanner = new Scanner(file);
-			
 			while(scanner.hasNext()) {
 				String text = (scanner.nextLine());
-				System.out.println("Text in file " +file +": " +text);
-				
 				boolean val = text.contains(searchText);
-				if(val)
-					System.out.println("String found: "+searchText);
-				else 
-					System.out.println("string not found");
+				if(val) {
+					System.out.println(filepath);
+				}
 			}
 		} catch (FileNotFoundException e) {
-			System.out.println("Filen finns inte");
+			System.out.println("sorry, file does not exist");
 		} finally {
 			if (scanner != null) {
 				scanner.close(); 
 			}
-			
 		}
 	}
-
 }
