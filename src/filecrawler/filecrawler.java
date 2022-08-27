6619211e7e51d2a0c709bcb21b715851993d3177
mhema <mhema@192.168.7.120>
2022-08-27 23:13:19 +0200

    input search text, read file, search text in file

--- src/filecrawler/filecrawler.java
+++ src/filecrawler/filecrawler.java
@@ -1,16 +1,54 @@
 package filecrawler;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class filecrawler {
 	
 	public static void main(String[] args) {
 		
-		readFile("c:\\temp\röd.txt");
+		Scanner inp = null;
+		String searchText = null;
+		
+		inp = new Scanner(System.in);
+		System.out.print("Enter searchtext - ");  
+		searchText= inp.next();
+	
+		if (inp != null) {
+			inp.close();
+		}
+		
+		readFile("c:\\filecrawler\\röd.txt", searchText);
 		
 	}
-
-	private static void readFile(String string) {
-		// TODO Auto-generated method stub
+	
+	protected static void readFile(String filepath, String searchText) {
+		File file = new File(filepath);
 		
+		Scanner scanner = null;
+		
+		try {
+			scanner = new Scanner(file);
+			
+			while(scanner.hasNext()) {
+				String text = (scanner.nextLine());
+				System.out.println("Text in file " +file +": " +text);
+				
+				boolean val = text.contains(searchText);
+				if(val)
+					System.out.println("String found: "+searchText);
+				else 
+					System.out.println("string not found");
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Filen finns inte");
+		} finally {
+			if (scanner != null) {
+				scanner.close(); 
+			}
+			
+		}
 	}
 
 }
