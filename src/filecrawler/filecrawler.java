195212ccae259048c5c8a36b8d605573626844f6
mhema <mhema@192.168.7.120>
2022-08-27 16:11:42 +0200

    first commit

---
+++ src/filecrawler/filecrawler.java
@@ -0,0 +1,16 @@
+package filecrawler;
+
+public class filecrawler {
+	
+	public static void main(String[] args) {
+		
+		readFile("c:\\temp\röd.txt");
+		
+	}
+
+	private static void readFile(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
